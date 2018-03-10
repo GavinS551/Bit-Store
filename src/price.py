c45f4040f1c69aa9dad06b9a9c12376b1daa08bb
Rory Shaughnessy <roryshaughnessy111@gmail.com>
2018-03-10 21:06:24 +0000

    Added Coinmarketcap price api

--- src/price.py
+++ src/price.py
@@ -1,7 +1,22 @@
 import requests
 
+
 class Coinmarketcap:
-    pass
+
+    @staticmethod
+    def bitcoin_price(currency):
+        valid_currencies = ["AUD", "BRL", "CAD", "CHF", "CLP", "CNY", "CZK",
+                            "DKK", "EUR", "GBP", "HKD", "HUF", "IDR", "ILS",
+                            "INR", "JPY", "KRW", "MXN", "MYR", "NOK", "NZD",
+                            "PHP", "PKR", "PLN", "RUB", "SEK", "SGD", "THB",
+                            "TRY", "TWD", "ZAR"]
+        if currency.upper() in valid_currencies:
+            url = 'https://api.coinmarketcap.com/v1/ticker/bitcoin/?convert='
+            data = requests.get(url + currency).json()[0]
+            return data[f'price_{currency.lower()}']
+        else:
+            raise ValueError('Currency ticker not valid')
+
 
 class Bitfinex:
     pass
