a5761f5129718e754ba630c7669b4648592857ef
Rory Shaughnessy <roryshaughnessy111@gmail.com>
2018-03-11 16:39:25 +0000

    Ability to get specific address data and balance of an address in blockchain.py

--- src/blockchain.py
+++ src/blockchain.py
@@ -7,7 +7,9 @@ class AddressData:
         # Addresses should be passed as a list
         self.addresses = addresses
         self.raw_data = {}
-        # balance of all addresses
+
+        self._get_api_data()
+        # balance of all addresses im self.addresses
         self.wallet_balance = self.raw_data['wallet']['final_balance']
 
     def _get_api_data(self):
@@ -20,17 +22,20 @@ class AddressData:
 
     def _get_address_data(self, address):
         """ Returns *specific* address data """
-        pass
+        if address in self.addresses:
+            for i in range(len(self.addresses)):
+                if self.raw_data['addresses'][i]['address'] == address:
+                    return self.raw_data['addresses'][i]
 
     def get_address_balance(self, address):
         """ Returns balance in satoshis """
         if address in self.addresses:
-            pass
+            return self._get_address_data(address)['final_balance']
         else:
             raise ValueError('Address not in self.addresses')
 
 
 if __name__ == '__main__':
-
-    ad = AddressData(['37uP4pDfbbgBh6WAkmMeA4KNjyqUY5L544',
-                     '32Q3Eb6NxrShRAUCzAd3wARVvRm5M9o5F5'])
+    ad = AddressData(["32Q3Eb6NxrShRAUCzAd3wARVvRm5M9o5F5", "37uP4pDfbbgBh6WAkmMeA4KNjyqUY5L544"])
+    print(ad.get_address_balance('32Q3Eb6NxrShRAUCzAd3wARVvRm5M9o5F5'))
+    print(ad._get_address_data( "37uP4pDfbbgBh6WAkmMeA4KNjyqUY5L544"))
