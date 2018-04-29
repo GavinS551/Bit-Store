fc5eeaffc709169dd51ea3e70e537c28f3f9f70c
Gavin Shaughnessy <gavinshaughnessy@gmail.com>
2018-04-29 20:49:32 +0100

    refactored config.py and added a function to change config vars

--- src/config.py
+++ src/config.py
@@ -46,24 +46,40 @@ DEFAULT_CONFIG = {
 }
 
 
+def init():
+    # creates program data dir if it doesn't exist
+    if not os.path.isdir(DATA_DIR):
+        os.makedirs(DATA_DIR, exist_ok=True)
+
+    # and likewise for the config file itself
+    if not os.path.exists(CONFIG_FILE):
+        with open(CONFIG_FILE, 'w+') as cf:
+            json.dump(DEFAULT_CONFIG, cf, indent=4, sort_keys=False)
+
+
+def read_file():
+    with open(CONFIG_FILE, 'r') as c:
+        return json.load(c)
+
+
 def reset_config_file():
     with open(CONFIG_FILE, 'w') as cf:
         json.dump(DEFAULT_CONFIG, cf, indent=4, sort_keys=False)
 
 
-# creates program data dir if it doesn't exist
-if not os.path.isdir(DATA_DIR):
-    os.makedirs(DATA_DIR, exist_ok=True)
+def write_value(key, value):
+    if key not in DEFAULT_CONFIG:
+        raise ValueError(f'{key} is an invalid key')
+
+    config = read_file()
+    config[key] = value
+
+    with open(CONFIG_FILE, 'w') as cf:
+        json.dump(config, cf, indent=4, sort_keys=False)
 
 
-# and likewise for the config file itself
-if not os.path.exists(CONFIG_FILE):
-    with open(CONFIG_FILE, 'w+') as cf:
-        json.dump(DEFAULT_CONFIG, cf, indent=4, sort_keys=False)
-
-
-with open(CONFIG_FILE, 'r') as cf:
-    _CONFIG_VARS = json.load(cf)
+init()
+_CONFIG_VARS = read_file()
 
 
 # CONFIG VARIABLES
