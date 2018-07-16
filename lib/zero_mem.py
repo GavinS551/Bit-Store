9a827cb0a7277768e91220477697fea65df0527f
Gavin Shaughnessy <gavinshaughnessy@gmail.com>
2018-07-16 19:09:20 +0100

    added check for windows os in zero_mem.py

---
+++ lib/zero_mem.py
@@ -0,0 +1,19 @@
+""" Taken from <https://tinyurl.com/y7tq3963> """
+import sys
+import ctypes
+import platform
+
+
+def zeromem(string):
+    if platform.system() == 'Windows':
+        location = id(string) + 20
+        size = sys.getsizeof(string) - 20
+
+        memset = ctypes.cdll.msvcrt.memset
+        # For Linux, use the following. Change the 6 to whatever it is on your computer.
+        # memset =  ctypes.CDLL("libc.so.6").memset
+
+        memset(location, 0, size)
+
+    else:
+        print('This feature is only supported on Windows')
