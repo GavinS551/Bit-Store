66441e50fcc6fe4cc2cb5e5b4dd54a4636de7696
Gavin Shaughnessy <gavinshaughnessy@gmail.com>
2018-07-31 21:04:06 +0200

    renamed method

--- lib/data.py
+++ lib/data.py
@@ -114,6 +114,6 @@ class DataStore(Crypto):
 
     # for use outside this class, where the password isn't actually used
     # to decrypt the file, but still needs to be verified for security
-    def validate_password_hash(self, password):
+    def validate_password(self, password):
         return hashlib.sha256(password.encode('utf-8')).hexdigest() \
                == self.get_value(key='PASSWORD_HASH')
