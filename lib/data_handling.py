c2b4261d12c8b0bac04e5d0e1bd51a45b31c6581
Gavin Shaughnessy <gavinshaughnessy@gmail.com>
2018-08-08 23:28:36 +0100

    added dataclass to handle utxos in standard format

---
+++ lib/data_handling.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass
+
+@dataclass
+class UTXOData:
+    txid: str
+    output_num: int
+    address: str
+    script: str
+    value: int
+    confirmations: int
+
+    def is_confirmed(self):
+        return self.confirmations > 0
+
+    def standard_format(self):
+        """ returns data in standard format as shown in blockchain.py """
+        return self.txid, self.output_num, self.address, self.script, self.value, self.confirmations
