ed5cb89dba0f78d26aa4bd07269ac69101c128ff
Gavin Shaughnessy <gavinshaughnessy@gmail.com>
2018-08-23 23:39:27 +0100

    txid is returned from boradcast_transaction

--- lib/core/blockchain.py
+++ lib/core/blockchain.py
@@ -12,7 +12,7 @@ def broadcast_transaction(hex_transaction):
 
     request = requests.post(url=url, data={'tx_hex': hex_transaction})
 
-    return request.json() if request.ok else False
+    return request.json()['txid'] if request.ok else False
 
 
 def blockchain_api(addresses, refresh_rate, source):
