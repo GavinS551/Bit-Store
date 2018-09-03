99dbbfc7c77c5f8d527ae73e9f90508e7852af19
Gavin Shaughnessy <gavinshaughnessy@gmail.com>
2018-09-03 20:21:21 +0100

    added logging capabilities

---
+++ lib/logger.py
@@ -0,0 +1,68 @@
+import logging
+import os
+import datetime
+
+from .core import config
+
+
+def get_logger():
+
+    # create dir for log files if it doesn't exist
+    if not os.path.isdir(config.LOGGER_DIR):
+        os.makedirs(config.LOGGER_DIR, exist_ok=True)
+
+    formatter = logging.Formatter(fmt='%(asctime)s : %(levelname)s : %(module)s : %(message)s')
+
+    handler = _FileHandlerManager(log_dir=config.LOGGER_DIR,
+                                  max_logs=config.MAX_LOG_FILES_STORED).get_file_handler()
+    handler.setFormatter(formatter)
+
+    logger = logging.getLogger('root')
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(handler)
+
+    return logger
+
+
+class _FileHandlerManager:
+    """ log files are named starting with self.file_prefix, and appending
+    a unix timestamp rounded to nearest second. Use the method get_file_handler
+    to return a FileHandler object to be used with logging module
+    """
+
+    def __init__(self, log_dir, max_logs=10):
+        self.log_dir = log_dir
+        self.max_logs = max_logs
+
+        self.file_prefix = 'LOG_'
+
+        self.age_sorted_files = []
+
+    def full_log_path(self, log):
+        return os.path.join(self.log_dir, log)
+
+    @property
+    def logs(self):
+        return [log for log in os.listdir(self.log_dir) if log.startswith(self.file_prefix)]
+
+    @property
+    def time_sorted_logs(self):
+
+        def _sort_key(log):
+            timestamp = int(log[len(self.file_prefix):])
+            return timestamp
+
+        return sorted(self.logs, key=_sort_key)
+
+    def get_file_handler(self):
+        name = self.file_prefix + str(round(datetime.datetime.now().timestamp()))
+        full_path = self.full_log_path(name)
+
+        # remove the oldest log file if there are more than max logs
+        if len(self.logs) > self.max_logs:
+            os.remove(self.full_log_path(self.time_sorted_logs[0]))
+
+        with open(full_path, 'w+'):
+            pass
+
+        return logging.FileHandler(self.full_log_path(name))
--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 from lib.gui import gui_root
+from lib import logger
 
 
 if __name__ == '__main__':
+    root_logger = logger.get_logger()
+    root_logger.info('Created root logger.')
+
+    root_logger.info('Starting gui_root mainloop.')
     gui_root.main()
+
+    root_logger.info('Exiting gui_root mainloop.')
