623480fadaf31cb2d7f9658f256a9275174c994b
Gavin Shaughnessy <gavinshaughnessy@gmail.com>
2018-09-06 21:37:46 +0100

    build.py now uses sys.executable to find correct python path

--- build.py
+++ build.py
@@ -1,12 +1,16 @@
 import os
 import platform
+import sys
 
-if platform.system() == 'Windows':
-    os.system(r'py -3.6 -m PyInstaller main.py -w -i lib/gui/assets/bc_logo.ico '
-              r'-p lib -F -n Bit-Store.exe --add-data lib/core/wordlist.txt;lib/core '
-              r'--add-data lib/gui/assets/bc_logo.ico;lib/gui/assets')
 
-elif platform.system() == 'Linux':
-    os.system(r'python3.6 -m PyInstaller main.py -w -i lib/gui/assets/bc_logo.ico '
-              r'-p lib -F -n Bit-Store --add-data lib/core/wordlist.txt:lib/core '
-              r'--add-data lib/gui/assets/bc_logo.ico:lib/gui/assets --hidden-import="PIL._tkinter_finder"')
+if __name__ == '__main__':
+
+    if platform.system() == 'Windows':
+        os.system(f'"{sys.executable}" -m PyInstaller main.py --debug -i lib/gui/assets/bc_logo.ico '
+                  '-p lib -F -n Bit-Store.exe --add-data lib/core/wordlist.txt;lib/core '
+                  '--add-data lib/gui/assets/bc_logo.ico;lib/gui/assets')
+
+    elif platform.system() in ('Linux', 'Darwin'):
+        os.system(f'"{sys.executable}" -m PyInstaller main.py -w -i lib/gui/assets/bc_logo.ico '
+                  '-p lib -F -n Bit-Store --add-data lib/core/wordlist.txt:lib/core '
+                  '--add-data lib/gui/assets/bc_logo.ico:lib/gui/assets --hidden-import="PIL._tkinter_finder"')
