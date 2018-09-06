3d4e02ae380b8b8b2857007a2fb061f471651021
Gavin Shaughnessy <gavinshaughnessy@gmail.com>
2018-09-06 19:33:01 +0100

    fixed hidden import

--- build.py
+++ build.py
@@ -9,4 +9,4 @@ if platform.system() == 'Windows':
 elif platform.system() == 'Linux':
     os.system(r'python3.6 -m PyInstaller main.py -w -i lib/gui/assets/bc_logo.ico '
               r'-p lib -F -n Bit-Store --add-data lib/core/wordlist.txt:lib/core '
-              r'--add-data lib/gui/assets/bc_logo.ico:lib/gui/assets')
+              r'--add-data lib/gui/assets/bc_logo.ico:lib/gui/assets --hidden-import="PIL._tkinter_finder"')
