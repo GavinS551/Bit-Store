d89f18b91d8d6fa2bb35337298d833cb60b786d8
Gavin Shaughnessy <gavinshaughnessy@gmail.com>
2018-09-07 23:54:53 +0100

    removed use of globals()

--- lib/core/config.py
+++ lib/core/config.py
@@ -20,13 +20,16 @@ def init():
     missing_vars = set(DEFAULT_CONFIG) - {k for k in read_file() if k in DEFAULT_CONFIG}
     write_values(**{k: DEFAULT_CONFIG[k] for k in missing_vars})
 
-    set_config_vars()
-
 
 @functools.lru_cache(maxsize=None)
 def read_file():
-    with open(CONFIG_FILE, 'r') as c:
-        return json.load(c)
+    try:
+        with open(CONFIG_FILE, 'r') as c:
+            return json.load(c)
+    except FileNotFoundError:
+        # when config is imported, and there is no config file,
+        # just use what will be dumped into it anyway on init() call
+        return DEFAULT_CONFIG
 
 
 def write_values(**kwargs):
@@ -43,6 +46,22 @@ def write_values(**kwargs):
     utils.atomic_file_write(data, CONFIG_FILE)
 
 
-def set_config_vars():
-    for k, v in read_file().items():
-        globals()[k] = v
+BIP32_PATHS = read_file()['BIP32_PATHS']
+
+PRICE_API_SOURCE = read_file()['PRICE_API_SOURCE']
+
+BLOCKCHAIN_API_SOURCE = read_file()['BLOCKCHAIN_API_SOURCE']
+
+FIAT = read_file()['FIAT']
+
+BTC_UNITS = read_file()['BTC_UNITS']
+
+FONT = read_file()['FONT']
+
+SPEND_UNCONFIRMED_UTXOS = read_file()['SPEND_UNCONFIRMED_UTXOS']
+
+SPEND_UTXOS_INDIVIDUALLY = read_file()['SPEND_UTXOS_INDIVIDUALLY']
+
+MAX_LOG_FILES_STORED = read_file()['MAX_LOG_FILES_STORED']
+
+GUI_SHOW_FIAT_TX_HISTORY = read_file()['GUI_SHOW_FIAT_TX_HISTORY']
