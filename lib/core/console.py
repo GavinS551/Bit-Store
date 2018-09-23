523e32cf50ce4844ee18785d8ea58e51019717c5
Gavin Shaughnessy <gavinshaughnessy@gmail.com>
2018-09-23 23:45:32 +0100

    added console metaclass to automatically handle errors caused by invalid arguments

--- lib/core/console.py
+++ lib/core/console.py
@@ -8,19 +8,79 @@ import contextlib
 import io
 import traceback
 import inspect
+import functools
+import types
 
 
 class IncorrectArgsError(Exception):
-
-    def __init__(self, cmd, n_expected_args, n_passed_args, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        self.cmd = cmd
-        self.n_expected_args = n_expected_args
-        self.n_passed_args = n_passed_args
+    """ when raised in a 'do' method in the console class, the below wrapper will
+    automatically show the user the required args and their type annotations (if present)
+    in the exception string.
+    """
+    pass
 
 
-class Console:
+class ConsoleArgErrorsMeta(type):
+
+    def __new__(mcs, name, bases, attrs):
+
+        for name, value in attrs.items():
+            if isinstance(value, types.FunctionType) and name.startswith('do_'):
+                attrs[name] = mcs._args_error_info_decorate(value)
+
+        return super().__new__(mcs, name, bases, attrs)
+
+    @staticmethod
+    def _args_error_info_decorate(func):
+
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+
+            try:
+                func(*args, **kwargs)
+
+            except (IncorrectArgsError, TypeError):
+
+                sig = inspect.signature(func)
+
+                cmd_name = func.__name__[len('do_'):]
+                num_args = len(sig.parameters)
+
+                if 'self' in sig.parameters:
+                    num_args -= 1
+
+                arg_annos = []
+                for n, p in sig.parameters.items():
+                    if p.annotation != sig.empty:
+                        arg_annos.append((n, p.annotation.__name__))
+                    else:
+                        arg_annos.append((n, None))
+
+                str_arg_annos = ''
+                for i, (arg_name, anno) in enumerate(arg_annos, start=1):
+                    # ignore self
+                    if arg_name == 'self':
+                        continue
+
+                    if anno is not None:
+                        str_arg_annos += f"'{arg_name}': {anno}"
+                    else:
+                        str_arg_annos += f"'{arg_name}'"
+
+                    # concatenate the comma if it isn't the end of the list
+                    if i < len(arg_annos):
+                        str_arg_annos += ', '
+
+                err_str = f'Error: "{cmd_name}" expects {num_args} arguments'
+                if num_args > 0:
+                    err_str += f': {str_arg_annos}'
+
+                raise IncorrectArgsError(err_str)
+
+        return wrapper
+
+
+class Console(metaclass=ConsoleArgErrorsMeta):
     """
     This class should be sub-classed. You defined methods called do_{something},
     and when the string "{something}" is passed into exec_cmd, the method do_{something}
@@ -28,10 +88,17 @@ class Console:
     the method's docstring, which is usually enough. More complicated help commands can be
     created by defining a help_{something} method that will be called instead. The default do_help
     will list all possible do_ commands.
+
+    TODO: add info about IncorrectArgsError Handling
     """
 
-    def __init__(self, intro=None):
-        self._output = io.StringIO()
+    def __init__(self, intro=None, stdout=None):
+        # if stdout is None, it can be accessed by the output property
+        # and will be assigned to a StringIO object
+        if stdout is None:
+            self._output = io.StringIO()
+        else:
+            self._output = stdout
 
         if intro is not None:
             self._output.write(intro)
@@ -45,7 +112,8 @@ class Console:
         return self._output.getvalue()
 
     def clear_output(self):
-        self._output = io.StringIO()
+        self._output.seek(0)
+        self._output.truncate()
 
     @staticmethod
     def fallback_cmd():
@@ -56,7 +124,7 @@ class Console:
          if not already present. (default help_cmd is print(inspect.getdoc(self.do_{cmd})) )
         """
 
-        make_helper = lambda method: lambda: print(inspect.getdoc(getattr(self, f'do_{method}')))
+        make_helper = lambda method: lambda: print(f"{method}: {inspect.getdoc(getattr(self, f'do_{method}'))}")
 
         do_methods = {m[len('do_'):] for m in dir(self) if callable(getattr(self, m))
                       and m.startswith('do_')}
@@ -85,6 +153,8 @@ class Console:
         # redirect stdout to self.output
         with contextlib.redirect_stdout(self._output):
             try:
+                cmd_attr = getattr(self, f'do_{cmd}')
+
                 if print_cmd:
                     print(cmd, *args)
 
@@ -94,12 +164,10 @@ class Console:
 
                 # if it has args, then pass into do_help
                 elif cmd in ('?', 'help') and args:
-                    if len(args) > 1:
-                        raise IncorrectArgsError('help', 1, len(args))
-                    getattr(self, f'help_{args[0]}')()
+                    self.do_help(cmd=args[0])
 
                 else:
-                    getattr(self, f'do_{cmd}')(*args)
+                    cmd_attr(*args)
 
             except AttributeError:
                 if default is not None:
@@ -108,15 +176,20 @@ class Console:
                     self.fallback_cmd()
 
             except IncorrectArgsError as ex:
-                print(f'Error: {ex.cmd} command expects {ex.n_expected_args} arg(s) but received {ex.n_passed_args}')
+                print(str(ex))
 
             # print any other unhandled exception to output
             except Exception:
                 print(traceback.format_exc())
 
     # if a cmd_name is passed in, a specific self.help_function will be called
-    def do_help(self):
+    def do_help(self, cmd=None):
         """ Displays all possible commands. """
-        print('Possible Commands:')
-        print('(Use "help <command>" for specific information)\n')
-        print('\n'.join([d[len('do_'):] for d in dir(self) if callable(getattr(self, d)) and d.startswith('do_')]))
+
+        if cmd is not None:
+            getattr(self, f'help_{cmd}')()
+
+        else:
+            print('Possible Commands:')
+            print('(Use "help <command>" for specific information)\n')
+            print('\n'.join([d[len('do_'):] for d in dir(self) if callable(getattr(self, d)) and d.startswith('do_')]))
--- lib/gui/main_wallet/_console_display.py
+++ lib/gui/main_wallet/_console_display.py
@@ -1,10 +1,72 @@
 import tkinter as tk
 from tkinter import ttk
 
-from ...core import console
+import io
+
+from ...core import console, utils
+
+
+class GUIConsoleOutput(io.IOBase):
+    """ to support a constant output display, not waiting for the command
+     to be fully complete, then display output to Text widget
+    """
+    prompt_text = '>> '
+
+    def __init__(self, tk_text, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.tk_text = tk_text
+        self.tk_text['state'] = tk.DISABLED
+
+    def write(self, text):
+        self.tk_text['state'] = tk.NORMAL
+
+        new_text = text.replace('\n', f'\n{self.prompt_text}')
+
+        self.tk_text.insert(tk.END, new_text)
+        self.tk_text.see('end')
+
+        self.tk_text['state'] = tk.DISABLED
+
+    def getvalue(self):
+        self.tk_text['state'] = tk.NORMAL
+        val = self.tk_text.get(1.0, 'end-1c')
+        self.tk_text['state'] = tk.DISABLED
+
+        return val
+
+    def seek(self, *args, **kwargs):
+        pass
+
+    def read(self):
+        pass
+
+    def truncate(self, *args, **kwargs):
+        self.tk_text['state'] = tk.NORMAL
+        self.tk_text.delete(1.0, tk.END)
+        self.tk_text.insert(tk.END, self.prompt_text)
+        self.tk_text['state'] = tk.DISABLED
+
+
+class GUIConsole(console.Console):
+
+    def __init__(self, root, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.root = root
+        self.wallet = self.root.btc_wallet
+
+    def do_clear(self):
+        self.clear_output()
+
+    def do_addresses(self):
+        """ Prints all wallet addresses. """
+        print(self.wallet.all_addresses)
 
 
 class ConsoleDisplay(ttk.Frame):
+
+    intro = 'Bit-Store Console. Please type ? or "help" for a list of possible commands.\n'
+    console_font = ('consolas', 10)
 
     def __init__(self, master, main_wallet):
         ttk.Frame.__init__(self, master, padding=5)
@@ -15,19 +77,31 @@ class ConsoleDisplay(ttk.Frame):
         self.grid_columnconfigure(0, weight=1)
         self.grid_rowconfigure(0, weight=1)
 
-        self.console = None
-
-        self.console_text = tk.Text(self, height=10, wrap=tk.WORD, font=self.root.small_font,
+        self.console_text = tk.Text(self, height=10, wrap=tk.WORD, font=self.console_font,
                                     state=tk.DISABLED)
         self.console_text.configure(inactiveselectbackground=self.console_text.cget("selectbackground"))
         self.console_text.grid(row=0, column=0, sticky='nsew')
 
-        self.command_entry = ttk.Entry(self)
-        self.command_entry.grid(row=1, column=0, pady=(5, 0), sticky='ew')
+        self.console_entry = ttk.Entry(self)
+        self.console_entry.grid(row=1, column=0, pady=(5, 0), sticky='ew')
 
-        self.command_entry.bind('<Return>', )
+        self._custom_out = GUIConsoleOutput(self.console_text)
+        self.console = GUIConsole(root=self.root, intro=self.intro, stdout=self._custom_out)
 
-    def write_console_text(self, text):
-        self.console_text['state'] = tk.NORMAL
-        self.console_text.insert(tk.END, text)
-        self.console_text['state'] = tk.DISABLED
+        self.console_entry.bind('<Return>', self.execute_command)
+
+    @utils.threaded(daemon=True, name='GUI_CONSOLE_THREAD')
+    def execute_command(self, event):
+        cmd = event.widget.get()
+        event.widget.delete(0, tk.END)
+
+        # disable entry while command is executing in thread
+        event.widget.insert(0, 'Executing, please wait...')
+        event.widget['state'] = tk.DISABLED
+
+        try:
+            self.console.exec_cmd(cmd)
+
+        finally:
+            event.widget['state'] = tk.NORMAL
+            event.widget.delete(0, tk.END)
