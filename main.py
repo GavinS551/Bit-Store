6d2e7dfc4cd754f4f78010de7ebc340cfb3f7a02
Gavin Shaughnessy <gavinshaughnessy@gmail.com>
2018-09-26 22:31:33 +0100

    all unhandled exceptions are caught in main, then logged and re-raised

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
+import traceback
+
 from lib import logger
 from lib.core import config
 from lib.gui import gui_root
 
 
 if __name__ == '__main__':
-    # setting all global config variables, making sure all program directories exist,
-    # and creating them otherwise.
+    # making sure all program directories exist, and creating them otherwise.
     config.init()
 
     root_logger = logger.get_logger()
     root_logger.info('Created root logger.')
 
-    root_logger.info('Starting gui_root mainloop.')
-    gui_root.main()
+    try:
+        root_logger.info('Starting gui_root mainloop.')
+        gui_root.main()
 
-    root_logger.info('Exiting gui_root mainloop.')
+        root_logger.info('Exiting gui_root mainloop.')
+
+    except BaseException:
+        root_logger.critical(traceback.format_exc())
+        raise
