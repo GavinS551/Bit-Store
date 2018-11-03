d2f4a13989cce64a81715bbce4bd798f5c099c61
Gavin Shaughnessy <gavinshaughnessy@gmail.com>
2018-11-03 19:25:37 +0000

    added copy button to receive display

--- lib/gui/main_wallet/_receive_display.py
+++ lib/gui/main_wallet/_receive_display.py
@@ -26,25 +26,38 @@ class ReceiveDisplay(ttk.Frame):
         ttk.Frame.__init__(self, master, padding=5)
         self.main_wallet = main_wallet
 
+        self.address_frame = ttk.Frame(self)
+        self.qr_frame = ttk.Frame(self)
+
         self.grid_rowconfigure(0, {'minsize': 10})
 
-        self.address_label = ttk.Label(self, text='Receiving Address:',
+        self.address_label = ttk.Label(self.address_frame, text='Receiving Address:',
                                        font=self.main_wallet.root.small_font + ('bold',))
         self.address_label.grid(row=1, column=0, sticky='w', padx=10)
 
-        self.address_text = tk.Text(self, height=1, width=40, font=self.main_wallet.root.small_font,
+        self.address_text = tk.Text(self.address_frame, height=1, width=40, font=self.main_wallet.root.small_font,
                                     state=tk.DISABLED)
         self.address_text.grid(row=1, column=1, padx=20)
+
+        self.copy_button = ttk.Button(self.address_frame, text='Copy', command=self._on_copy)
+        self.copy_button.grid(row=2, column=1, padx=20, pady=(20, 0))
 
         self.qr = None
         self.qr_label = None
         self.draw_qr_code()
 
+        self.address_frame.grid(row=1, column=0)
+        self.qr_frame.grid(row=1, column=1)
+
         self._update_address()
+
+    def _on_copy(self):
+        self.main_wallet.root.clipboard_clear()
+        self.main_wallet.root.clipboard_append(self.address)
 
     def _update_address(self):
         """ a text widget is used for address display as it is copyable, however
-         it doesn't have a textvariable param so the address is updated here
+        it doesn't have a textvariable param so the address is updated here
         """
         # if the address has changed, then update text
         if self.address_text.get(1.0, 'end-1c') != self.main_wallet.next_receiving_address.get():
@@ -71,8 +84,8 @@ class ReceiveDisplay(ttk.Frame):
     def draw_qr_code(self):
         # keep reference to image or it will be garbage collected
         self.qr = self._make_qr_code()
-        self.qr_label = ttk.Label(self, image=self.qr)
-        self.qr_label.grid(row=1, column=2)
+        self.qr_label = ttk.Label(self.qr_frame, image=self.qr)
+        self.qr_label.grid(row=0, column=0)
 
     @property
     def address(self):
